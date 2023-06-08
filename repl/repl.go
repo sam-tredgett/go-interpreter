6c0685d81acef6310e077a4bbf06d798ca9bb73b
dabi-ngin <satre@pop-os.localdomain>
2023-06-08 21:33:14 +0100

    unpacked project structure, added parsing for return statements

---
+++ repl/repl.go
@@ -0,0 +1,29 @@
+package repl
+
+import (
+	"Monkey/lexer"
+	"Monkey/token"
+	"bufio"
+	"fmt"
+	"io"
+)
+
+const PROMPT = ">> "
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+	for {
+		fmt.Fprintf(out, PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+
+		line := scanner.Text()
+		l := lexer.New(line)
+
+		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
+			fmt.Fprintf(out, "%+v\n", tok)
+		}
+	}
+}
