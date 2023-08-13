19c1661683d534cab5347b50066a3b7c800ff398
dabi-ngin <satre@pop-os.localdomain>
2023-08-13 15:25:53 +0100

    implemented prefix operator parsing

--- ast/ast.go
+++ ast/ast.go
@@ -25,6 +25,7 @@ type ExpressionStatement struct {
     Expression  Expression
 }
 
+
 type Program struct {
     Statements []Statement
 }
@@ -114,3 +115,26 @@ type IntegerLiteral struct {
 func (il *IntegerLiteral) expressionNode() {}
 func (il *IntegerLiteral) TokenLiteral() string { return il.Token.Literal }
 func (il *IntegerLiteral) String()       string { return il.Token.Literal }
+
+type PrefixExpression struct {
+    Token       token.Token
+    Operator    string
+    Right       Expression
+}
+
+func (pe *PrefixExpression) expressionNode() {}
+func (pe *PrefixExpression) TokenLiteral() string { return pe.Token.Literal }
+func (pe *PrefixExpression) String() string {
+    var out bytes.Buffer
+
+    out.WriteString("(")
+    out.WriteString(pe.Operator)
+    out.WriteString(pe.Right.String())
+    out.WriteString(")")
+
+    return out.String()
+}
+
+
+
+
