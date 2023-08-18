a04bf2cff448f0ba5c2e61d544b4188d699eef7d
dabi-ngin <satre@pop-os.localdomain>
2023-08-18 23:23:59 +0100

    implemented evaluation of prefix ! and - operators for integers and booleans

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -5,6 +5,12 @@ import (
 	"Monkey/object"
 )
 
+var (
+    NULL    = &object.Null{}
+    TRUE    = &object.Boolean{Value: true}
+    FALSE   = &object.Boolean{Value: false}
+)
+
 
 func Eval(node ast.Node) object.Object {
     switch node := node.(type) {
@@ -13,13 +19,22 @@ func Eval(node ast.Node) object.Object {
         return evalStatements(node.Statements)
 
 
+
     case *ast.ExpressionStatement:
         return Eval(node.Expression) 
 
+    // Expressions
+    case *ast.PrefixExpression:
+        right := Eval(node.Right)
+        return evalPrefixExpression(node.Operator, right)
+
     case *ast.IntegerLiteral:
         return &object.Integer{Value: node.Value}
-    }
+    
+    case *ast.Boolean:
+        return nativeBoolToBooleanObject(node.Value)
 
+    }
     return nil
 }
 
@@ -31,4 +46,44 @@ func evalStatements(stmts []ast.Statement) object.Object {
     }
 
     return result
+}
+
+func nativeBoolToBooleanObject(input bool) *object.Boolean {
+    if input {
+        return TRUE
+    }
+    return FALSE
+}
+
+func evalPrefixExpression(operator string, right object.Object) object.Object {
+    switch operator {
+    case "!":
+        return evalBangOperatorExpression(right)
+    case "-":
+        return evalMinusPrefixOperatorExpression(right)
+    default:
+        return NULL
+    }
+}
+
+func evalBangOperatorExpression(right object.Object) object.Object {
+    switch right {
+        case TRUE:
+            return FALSE
+        case FALSE:
+            return TRUE
+        case NULL:
+            return TRUE
+        default:
+            return FALSE
+    }
+}
+
+func evalMinusPrefixOperatorExpression(right object.Object) object.Object {
+    if right.Type() != object.INTEGER_OBJ {
+        return NULL
+    }
+
+    value := right.(*object.Integer).Value
+    return &object.Integer{Value: -value}
 }
