3f2cab2cbaaeb2feb48057090ef4f87863e9a7de
dabi-ngin <satre@pop-os.localdomain>
2023-08-18 22:58:07 +0100

    implemented beginning of evaluator, now able to evaluate out Integers in REPL

--- repl/repl.go
+++ repl/repl.go
@@ -1,6 +1,7 @@
 package repl
 
 import (
+	"Monkey/evaluator"
 	"Monkey/lexer"
 	"Monkey/parser"
 	"bufio"
@@ -41,9 +42,12 @@ func Start(in io.Reader, out io.Writer) {
             continue
         }
 
-        io.WriteString(out, program.String())
-        io.WriteString(out, "\n")
-	}
+        evaluated := evaluator.Eval(program)
+        if evaluated != nil {
+            io.WriteString(out, evaluated.Inspect())
+            io.WriteString(out, "\n")
+        }
+    }
 }
 
 func printParserErrors(out io.Writer, errors []string) {
