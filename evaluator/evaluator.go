a64ec3843ec6ad0580e0b5ad0b0ee93dc661f301
dabi-ngin <satre@pop-os.localdomain>
2023-08-20 00:02:53 +0100

    implemented evaluating return statements and nested return statements

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -16,12 +16,17 @@ func Eval(node ast.Node) object.Object {
     switch node := node.(type) {
 
     case *ast.Program:
-        return evalStatements(node.Statements)
+        return evalProgram(node)
 
     case *ast.ExpressionStatement:
         return Eval(node.Expression) 
+
     case *ast.BlockStatement:
-        return evalStatements(node.Statements)
+        return evalBlockStatement(node)
+
+    case *ast.ReturnStatement:
+        val := Eval(node.ReturnValue)
+        return &object.ReturnValue{Value: val}
 
     case *ast.IfExpression:
         return evalIfExpression(node)
@@ -47,13 +52,43 @@ func Eval(node ast.Node) object.Object {
     return nil
 }
 
+func evalProgram(program *ast.Program) object.Object {
+    var result object.Object
+
+    for _, statement := range program.Statements {
+        result = Eval(statement)
+
+        if returnValue, ok := result.(*object.ReturnValue); ok {
+            return returnValue.Value
+        }
+    }
+    return result
+}
+
 func evalStatements(stmts []ast.Statement) object.Object {
     var result object.Object
 
     for _, statement := range stmts {
         result = Eval(statement)
+
+        if returnValue, ok := result.(*object.ReturnValue); ok {
+            return returnValue.Value
+        }
     }
 
+    return result
+}
+
+func evalBlockStatement(block *ast.BlockStatement) object.Object {
+    var result object.Object
+
+    for _, statement := range block.Statements {
+        result = Eval(statement)
+
+        if result != nil && result.Type() == object.RETURN_VALUE_OBJ {
+            return result
+        }
+    }
     return result
 }
 
@@ -166,3 +201,4 @@ func isTruthy(obj object.Object) bool {
         return true
     }
 }
+
