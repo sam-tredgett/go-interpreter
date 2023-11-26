abadac37e1dfaef0f5434a7a1edc5b3c6448cd05
dabi ngin <sam.tredgett@gmail.com>
2023-11-26 16:18:57 +0000

    basic evaluation for strings

--- object/object.go
+++ object/object.go
@@ -13,6 +13,7 @@ const (
     INTEGER_OBJ = "INTEGER"
     BOOLEAN_OBJ = "BOOLEAN"
     NULL_OBJ    = "NULL"
+    STRING_OBJ  = "STRING"
     RETURN_VALUE_OBJ = "RETURN_VALUE"
     ERROR_OBJ = "ERROR"
     FUNCTION_OBJ = "FUNCTION"
@@ -80,3 +81,10 @@ func (f *Function) Inspect() string {
 
     return out.String()
 }
+
+type String struct {
+    Value string
+}
+
+func (s *String) Type() ObjectType { return STRING_OBJ }
+func (s *String) Inspect() string  { return s.Value }
