1deb5e8c1175ced6cf3bf90f025297c8a01abf16
dabi ngin <sam.tredgett@gmail.com>
2023-11-26 17:08:13 +0000

    implemented builtin function len

--- object/object.go
+++ object/object.go
@@ -17,6 +17,7 @@ const (
     RETURN_VALUE_OBJ = "RETURN_VALUE"
     ERROR_OBJ = "ERROR"
     FUNCTION_OBJ = "FUNCTION"
+    BUILTIN_OBJ = "BUILTIN"
 )
 
 type Object interface {
@@ -88,3 +89,13 @@ type String struct {
 
 func (s *String) Type() ObjectType { return STRING_OBJ }
 func (s *String) Inspect() string  { return s.Value }
+
+type BuiltInFunction func(args ...Object) Object 
+
+type Builtin struct {
+    Fn BuiltInFunction
+}
+
+func (b *Builtin) Type() ObjectType { return BUILTIN_OBJ }
+func (b *Builtin) Inspect() string { return "builtin function" }
+
